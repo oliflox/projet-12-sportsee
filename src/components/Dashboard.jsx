f9355fdd92c3b0e160807e733d558ba4d2077330
oliflox <83953551+oliflox@users.noreply.github.com>
2024-12-31 15:11:34 +0100

    Refactor Dashboard and useUser hook: rename userInfos to userData and update data fetching logic

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,14 +1,14 @@
-import { useUser} from "../hooks/useUser";
+import { useUser } from "../hooks/useUser";
 import Graphics from "./Graphic";
 import KeyData from "./KeyData";
 
 export default function Dashboard() {
-  const userInfos = useUser(12);
+  const userData = useUser(12);
 
   return (
     <section className="dashboard">
       <h2 className="dashboard-title">
-        Bonjour <span className="username">{userInfos?.firstName}</span>
+        Bonjour <span className="username">{userData?.userInfos.firstName}</span>
       </h2>
       <h3>Félicitation ! Vous avez explosé vos objectifs hier &#128079;</h3>
       <section className="dashboard-analytics">
--- src/hooks/useUser.jsx
+++ src/hooks/useUser.jsx
@@ -3,12 +3,14 @@ import { getUserUrls } from "../constants/BaseUrl";
 
 const useUser = (userId) => {
   const [userData, setUserData] = useState(null);
-  const { userUrl } = getUserUrls(userId);
+  const { mainUrl } = getUserUrls(userId);
 
   useEffect(() => {
+    if (!userId) return;
+
     const fetchData = async () => {
       try {
-        const response = await fetch(userUrl);
+        const response = await fetch(mainUrl);
         const result = await response.json();
         setUserData(result.data);
       } catch (error) {
@@ -17,7 +19,7 @@ const useUser = (userId) => {
     };
 
     fetchData();
-  }, [userUrl]);
+  }, [mainUrl, userId]);
 
   return userData;
 };
