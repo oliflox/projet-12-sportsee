2189394a629edad8f58d984504caa9f31f85b5f9
oliflox <83953551+oliflox@users.noreply.github.com>
2025-05-27 16:01:01 +0200

    Update App component to use environment variable for default user ID

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import VerticalNav from './components/layout/VerticalNav';
 import Dashboard from './components/Dashboard';
 import ErrorPage from './components/ErrorPage';
 
+const DEFAULT_USER_ID = import.meta.env.VITE_DEFAULT_USER_ID;
+
 const App = () => {
   return (
     <>
@@ -13,7 +15,7 @@ const App = () => {
       <section className='dashboard-container'>
         <VerticalNav />
         <Routes>
-          <Route path='/' element={<Navigate to='/user/12' replace />} />
+          <Route path='/' element={<Navigate to={`/user/${DEFAULT_USER_ID}`} replace />} />
           <Route path='/user/:userId' element={<Dashboard />} />
           <Route path='/error/:errorType' element={<ErrorPage />} />
           <Route path='*' element={<ErrorPage errorType='NOT_FOUND' />} />
